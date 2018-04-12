d0a8cca5860f968f87b4a1a5a5b2832e3f9ed09b
jirenz747 <37732694+jirenz747@users.noreply.github.com>
2018-04-12 13:30:09 +0300

    Create passwords.py

---
+++ passwords.py
@@ -0,0 +1,12 @@
+def get_network_device_password():
+    LOGIN = 'login'
+    PASSWORD = 'password'
+    ENABLE_PASSWORD = 'enable_password'
+    return [LOGIN, PASSWORD, ENABLE_PASSWORD]
+    
+def get_ip_address():
+    IP_COD_BEELINE = '1.1.1.1' 
+    IP_COD_PROSTOR = '2.2.2.2'
+    IP_COD_DOMRU = '3.3.3.3'
+    IP_COD_INET = '4.4.4.4'
+    return [IP_COD_BEELINE, IP_COD_PROSTOR, IP_COD_DOMRU, IP_COD_INET]
